602974f9f4e89af5202f73e13c285235753052ee
Valesca Ramos <valescaramos15@gmail.com>
2024-05-14 12:36:14 -0300

    Final

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,23 +1,33 @@
 function showAlert(text) {
-    alert(text);
+	alert(text);
 }
 //Services page
-    /* Animação toggle nos tópicos */
-    //Seleciona os botões e os textos dos serviços
-    const toggleButtons = document.querySelectorAll('[id^=toggle_]');
-    const serviceTexts = document.querySelectorAll('[id^=serviceText_]');
+/* Animação toggle nos tópicos */
+//Seleciona os botões e os textos dos serviços
+const toggleButtons = document.querySelectorAll("[id^=toggle_]");
+const serviceTexts = document.querySelectorAll("[id^=serviceText_]");
 
-    // Adiciona evento de clique a cada botão
-    toggleButtons.forEach((button, index) => {
-        button.addEventListener('click', function () {
-            // Verifica se o texto do serviço está visível
-            if (serviceTexts[index].style.display === 'none') {
-                // Se estiver invisível, torna-o visível
-                serviceTexts[index].style.display = 'block';
-            } else {
-                // Caso contrário, torna-o invisível
-                serviceTexts[index].style.display = 'none';
-            }
-        });
-    });
-    
+// Adiciona evento de clique a cada botão
+toggleButtons.forEach((button, index) => {
+	button.addEventListener("click", function () {
+		// Verifica se o texto do serviço está visível
+		if (serviceTexts[index].style.display === "none") {
+			// Se estiver invisível, torna-o visível
+			serviceTexts[index].style.display = "block";
+		} else {
+			// Caso contrário, torna-o invisível
+			serviceTexts[index].style.display = "none";
+		}
+	});
+});
+
+#TODO: JS sem ação
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+	anchor.addEventListener("click", function (e) {
+		e.preventDefault();
+
+		document.querySelector(this.getAttribute("href")).scrollIntoView({
+			behavior: "smooth",
+		});
+	});
+});
